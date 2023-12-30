b33ee9e351db62e34dc794981386004c5b5e6abe
Hobar <huparelaa@eafit.edu.co>
2023-12-30 12:50:25 -0500

    solved: challenge 2

--- src/utils/projectList.js
+++ src/utils/projectList.js
@@ -4,5 +4,11 @@ export const PROJECTSLIST = [
         day: "01",
         link: "projects/01",
         imagePath: "/public/01.png"
+    },
+    {
+        name: "Progress steps",
+        day: "02",
+        link: "projects/02",
+        imagePath: "/public/02.png"
     }
 ]
